71cadeea8c7151d5542e6136651ed23c0dc6d2d8
nadunhetti <nadunpankaja123@gmail.com>
2025-08-20 19:07:24 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: ":https://nadunhetti.github.io/trend-canvas-online/:",
+    host: "::",
     port: 8080,
   },
   plugins: [
