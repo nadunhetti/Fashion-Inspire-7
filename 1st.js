fd5d28317f4e5919877fa43f8f98d7e2d0e0725a
Nadun <nadunhettiarachchi77@gmail.com>
2026-05-14 11:05:06 +0530

    Add initial HTML and JavaScript files with basic structure and functionality

---
+++ 1st.js
@@ -0,0 +1,14 @@
+for (let i = 0; i <= 2; i++) {
+  for (let j = 1; j <= 2; j++) {
+    console.log(`Name:${i}
+    Age:${j}`);
+  }
+}
+let a=0;
+
+while (a<3){        //while loop
+  console.log(`Count:${a}`);
+  a++;
+
+
+}
